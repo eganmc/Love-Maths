a3faf6e44c4bcf3e21d9f9d613de1b0cdca95c59
eganmc <eganmc@outlook.com>
2025-06-12 16:16:50 +0100

    added functionality to the scoreboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -34,8 +34,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey, good job bro!");
+        incrementScore();
     } else {
         alert(`Aww, you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}.`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -55,20 +57,19 @@ function calculateCorrectAnswer() {
 }
 
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
-    let correctAnswer = operand1 + operand2;
-    document.getElementById("correct-answer").setAttribute("data-answer", correctAnswer);
-
 }
 
 function displaySubtractQuestion() {
